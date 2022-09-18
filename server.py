27ded110dea239fd9e352bfb361e4a9fd4f1dc0e
Gustavo Basulto <gbasulto.guzman@gmail.com>
2022-09-18 20:17:01 -0300

    carga tarea

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from sqlite3 import DateFromTicks
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -5,10 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout/', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    nombre=request.form['first_name']
+    apellido=request.form['last_name']
+    nro_tarjeta=request.form['student_id']
+    strawberry=request.form['strawberry']
+    raspberry=request.form['raspberry']
+    apple=request.form['apple']
+    return render_template("checkout.html", nombre=nombre, apellido=apellido,nro_tarjeta=nro_tarjeta,apple=apple, raspberry=raspberry, strawberry=strawberry)
 
 @app.route('/fruits')         
 def fruits():
